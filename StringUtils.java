714ea74ae521841b08688a14598beeaf0e8dcc3e
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-11-26 19:24:13 +0200

    added  stringutils

---
+++ StringUtils.java
@@ -0,0 +1,16 @@
+public class StringUtils {
+    public static String toCamelCase(String input) {
+        String[] parts = input.split("_");
+        StringBuilder camelCase = new StringBuilder();
+
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i].toLowerCase();
+            if (i == 0)
+                camelCase.append(part);
+            else
+                camelCase.append(Character.toUpperCase(part.charAt(0))).append(part.substring(1));
+        }
+
+        return camelCase.toString();
+    }
+}
