6d60ce5f7f377984fc89eac6cf77517d8757f1b0
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2025-04-08 18:55:59 +0300

    bla

---
+++ Leetcode.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Leetcode {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n >= 0) System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+}
