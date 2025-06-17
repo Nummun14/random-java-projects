60c693e588822b543d230fc5af0b6bbeea6f0472
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2025-06-17 14:59:28 +0300

    Added myAtoi

--- Leetcode.java
+++ Leetcode.java
@@ -7,7 +7,7 @@ public class Leetcode {
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0)
+        if (s.isEmpty())
             return 0;
         int maxLength = 1;
         int currentLength = maxLength;
@@ -37,9 +37,33 @@ public class Leetcode {
         return combined.length % 2 == 0 ? (double) (combined[combined.length / 2] + combined[combined.length / 2 - 1]) / 2 : combined[combined.length / 2];
     }
 
+    static String DIGITS = "0123456789";
+
+    public static int myAtoi(String s) {
+        if (s.isEmpty())
+            return 0;
+        if (s.charAt(0) == ' ')
+            return myAtoi(s.substring(1));
+        StringBuilder answer = new StringBuilder(s.charAt(0) == '-' ? "-" : "");
+        if (s.charAt(0) == '-' || s.charAt(0) == '+')
+            s = s.substring(1);
+        if (s.isEmpty())
+            return 0;
+        if (!DIGITS.contains(s.substring(0, 1)))
+            return 0;
+        int i = 0;
+        while (i < s.length() && s.charAt(i) == '0')
+            i++;
+        while (i < s.length() && DIGITS.contains(s.substring(i, i + 1))) {
+            answer.append(s.charAt(i));
+            i++;
+        }
+        if (answer.length() > 11)
+            return answer.charAt(0) == '-' ? -2147483648 : 2147483647;
+        return answer.isEmpty() || answer.toString().equals("-") || answer.toString().equals("+") ? 0 : (int) Math.max(Math.min(Long.parseLong(answer.toString()), 2147483647), -2147483648);
+    }
+
     public static void main(String[] args) {
-        int[] a = {1, 2};
-        int[] b = {3, 4};
-        System.out.println(findMedianSortedArrays(a, b));
+        System.out.println(myAtoi("-2147483648"));
     }
 }
