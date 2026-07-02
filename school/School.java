061404010d878416da9dfffed6acf93592ee4cf7
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2026-07-02 14:45:27 +0300

    Refactor School.java main method and add utility functions
    Replace the main method in School.java with a brothers numbers checker algorithm. Add three new utility methods: isOrderedPlaces, isOrderedValues, and right. Clear Test.java of its queue-based test code.

--- school/School.java
+++ school/School.java
@@ -1,6 +1,51 @@
 package school;
 
+import java.util.Scanner;
+
 public class School {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        if (right(num1) == right(num2) && num1 % 10 == num2 % 10)
+            System.out.println("Numbers are brothers");
+        else
+            System.out.println("Numbers are not brothers");
+    }
+
+    public static boolean isOrderedPlaces(int[] arr) {
+        if (arr.length <= 2)
+            return true;
+        for (int i = 2; i < arr.length; i += 2) {
+            if (arr[i] < arr[i - 2])
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean isOrderedValues(int[] arr) {
+        if (arr.length <= 1)
+            return true;
+
+        int lastEven = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                if (arr[i] < lastEven)
+                    return false;
+                lastEven = arr[i];
+            }
+        }
+        return true;
+    }
+
+    public static int right(int num) {
+        while (num >= 10)
+            num /= 10;
+
+        return num;
+    }
+
     public static boolean isSquareRootWhole(int number) {
         int m = 0;
         while (m * m < number)
@@ -23,10 +68,5 @@ public class School {
         String s1 = first.substring(0, first.length() / 2);
         String s2 = second.substring(second.length() / 2);
         return s2 + s1;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(combine("oned", "thin"));
-        System.out.println(combineBetter("oned", "thin"));
     }
 }
